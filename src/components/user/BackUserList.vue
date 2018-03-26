be67cedc411a461204d410965bb8b1491f876dda
xue <15093568689@163.com>
2018-03-26 18:04:11 +0800

    用户管理 中途提交 待完善

--- src/components/user/BackUserList.vue
+++ src/components/user/BackUserList.vue
@@ -14,64 +14,87 @@
         <el-form-item>
           <el-button type="primary" @click="getUsers()">查询</el-button>
         </el-form-item>
+        <el-form-item style="float:right;">
+          <el-button type="primary" @click="handleAdd()">导入</el-button>
+        </el-form-item>
       </el-form>
     </el-col>
     <!-- table 内容 -->
     <el-table :data="orderList" style="width: 100%" :border='true'>
-      <el-table-column label="手机号" prop="OrderNo">
+      <el-table-column label="手机号" prop="Phone">
       </el-table-column>
-      <el-table-column label="头像" prop="OrderNo">
+      <el-table-column label="头像" prop="Image">
         <template slot-scope="scope">
-          <img v-for="(item,index) in scope.row.Image" :key="index" :src="mainurl+item" width="200" />
+          <img :src="mainurl+scope.row.Image" width="60" />
         </template>
       </el-table-column>
-      <el-table-column label="昵称" prop="OrderNo">
+      <el-table-column label="昵称" prop="Name">
       </el-table-column>
-      <el-table-column label="性别" prop="OrderNo">
-      </el-table-column>
-      <el-table-column label="生日" prop="OrderNo">
-      </el-table-column>
-      <el-table-column label="微信" prop="isChoice">
+      <el-table-column label="会员" prop="IsVip">
         <template slot-scope="scope">
-          <el-checkbox v-model="scope.row.Price"></el-checkbox>
-        </template>
-      </el-table-column>
-      <el-table-column label="QQ" prop="isChoice">
-        <template slot-scope="scope">
-          <el-checkbox v-model="scope.row.Price"></el-checkbox>
-        </template>
-      </el-table-column>
-      <el-table-column label="积分" prop="OrderNo">
-        <template slot-scope="scope">
-          <span>{{ scope.row.OrderNo }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="我的车辆" prop="OrderNo">
-        <template slot-scope="scope">
-          <el-popover ref="popover4" placement="right" width="400" trigger="click">
-            <el-table :data="gridData">
-              <el-table-column width="150" property="date" label="日期"></el-table-column>
-              <el-table-column width="100" property="name" label="姓名"></el-table-column>
-              <el-table-column width="300" property="address" label="地址"></el-table-column>
+          <el-popover ref="popover5" placement="right" width="400" trigger="click">
+            <el-table :data="carList">
+              <el-table-column width="200" property="Phone" label="折扣"></el-table-column>
+              <el-table-column width="200" property="Sex" label="减免"></el-table-column>
             </el-table>
           </el-popover>
-          <span v-popover:popover4 @click="detailClick">{{ scope.row.OrderNo }}</span>
+          <img v-if="!scope.row.IsVip" src="../../../static/images/vip.png"  v-popover:popover5 @click="vipClick(scope.$index, scope.row)"/>
         </template>
       </el-table-column>
-      <el-table-column label="我的月卡" prop="OrderNo">
+      <el-table-column label="性别" prop="Sex" :formatter="Sex">
+      </el-table-column>
+      <el-table-column label="生日" prop="Birthday">
+      </el-table-column>
+      <el-table-column label="微信" prop="WxOpenID">
         <template slot-scope="scope">
-          <span>{{ scope.row.OrderNo }}</span>
+          <el-checkbox v-model="scope.row.WxOpenID" disabled></el-checkbox>
         </template>
       </el-table-column>
-      <el-table-column label="我的优惠券" prop="OrderNo">
+      <el-table-column label="QQ" prop="QQOpenID">
         <template slot-scope="scope">
-          <span>{{ scope.row.OrderNo }}</span>
+          <el-checkbox v-model="scope.row.QQOpenID" disabled></el-checkbox>
+        </template>
+      </el-table-column>
+      <el-table-column label="积分" prop="Integration">
+      </el-table-column>
+      <el-table-column label="我的车辆" prop="Car">
+        <template slot-scope="scope">
+          <el-popover ref="popover1" placement="right" width="400" trigger="click">
+            <el-table :data="carList">
+              <el-table-column width="200" property="LicensePlate" label="车牌"></el-table-column>
+              <el-table-column width="200" property="IsAutomaticPayment" label="是否无感"></el-table-column>
+            </el-table>
+          </el-popover>
+          <span v-popover:popover1 @click="carClick(scope.$index, scope.row)">{{ scope.row.Car.length>0?"点击查看":"无" }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="我的月卡" prop="Card">
+        <template slot-scope="scope">
+          <el-popover ref="popover2" placement="right" width="400" trigger="click">
+            <el-table :data="cardList">
+              <el-table-column width="200" property="CardName" label="月卡名"></el-table-column>
+              <el-table-column width="200" property="data" label="有效期"></el-table-column>
+            </el-table>
+          </el-popover>
+          <span v-popover:popover2 @click="cardClick(scope.$index, scope.row)">{{ scope.row.Card.length>0?"点击查看":"无" }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="我的优惠券" prop="Coupon">
+        <template slot-scope="scope">
+          <el-popover ref="popover3" placement="right" width="500" trigger="click">
+            <el-table :data="CouponList">
+              <el-table-column width="100" property="Coupon" label="券名"></el-table-column>
+              <el-table-column width="150" property="full" label="满多少减多少"></el-table-column>
+              <el-table-column width="250" property="data" label="有效期"></el-table-column>
+            </el-table>
+          </el-popover>
+          <span v-popover:popover3 @click="couponClick(scope.$index, scope.row)">{{ scope.row.Coupon.length>0?"点击查看":"无" }}</span>
         </template>
       </el-table-column>
       <el-table-column label="操作">
         <template slot-scope="scope">
-          <el-button size="mini" v-if="scope.row.Status == 2" type="primary" plain icon="el-icon-success" @click="handleEdit(scope.$index, scope.row)">锁定</el-button>
-          <el-button size="mini" v-if="scope.row.Status == 1" type="danger" plain icon="el-icon-warning" @click="handleSend(scope.$index, scope.row)">解除锁定</el-button>
+          <el-button size="mini" v-if="scope.row.IsLock" type="primary" plain icon="el-icon-success" @click="handleEdit(scope.$index, scope.row)">解除锁定</el-button>
+          <el-button size="mini" v-else type="danger" plain icon="el-icon-warning" @click="handleEdit(scope.$index, scope.row)">点击锁定</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -85,136 +108,240 @@
   </div>
 </template>
 <script>
-  // import "../../../static/jquery-search.js";
-  import expresss from "../../../static/express.js";
-  export default {
-    data() {
-      return {
-        orderList: [{
-          OrderNo: 213213213,
-          isChoice: true
-        }], //列表
-        pageIndex: 1,
-        pageSize: 10,
-        pageCount: 1,
-        mainurl: "",
-        roleList: [], //管理员角色列表
-        // 搜索关键字
-        filters: {
-          keyword: ""
+export default {
+  data() {
+    return {
+      orderList: [], //列表
+      pageIndex: 1,
+      pageSize: 10,
+      pageCount: 1,
+      mainurl: "",
+      roleList: [], //管理员角色列表
+      // 搜索关键字
+      filters: {
+        keyword: ""
+      },
+      // 状态数组
+      typeList: [
+        {
+          name: "全部",
+          value: 0
         },
-        // 状态数组
-        typeList: [{
-            name: "全部",
-            value: 0
-          },
-          {
-            name: "待发货",
-            value: 1
-          },
-          {
-            name: "待收货",
-            value: 2
-          }
-        ],
-        // 我的车辆数组
-        gridData: []
-      };
+        {
+          name: "待发货",
+          value: 1
+        },
+        {
+          name: "待收货",
+          value: 2
+        }
+      ],
+      // 我的车辆数组
+      carList: [],
+      cardList: [],
+      CouponList: []
+    };
+  },
+  methods: {
+    carClick(index, row) {
+      var obj = Object.assign({}, row);
+      // obj.Car.forEach(element => {
+      //   element.IsAutomaticPayment = element.IsAutomaticPayment ? "是" : "否";
+      // });
+      this.carList = obj.Car;
     },
-    methods: {
-
-      detailClick() {
-        this.gridData = [{
-          date: '2016-05-02',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }, {
-          date: '2016-05-04',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }]
-      },
-      /*
-             1、获取列表 渲染列表
-             2、搜索关键字
-             3、分页
-          */
-      getInfo() {
-        this.$http
-          .post("/sps/api/Back/O_GetOrderList", {
-            Token: getCookie("token"),
-            PageIndex: this.pageIndex,
-            PageSize: this.pageSize,
-            Keyword: this.filters.keyword == "" ? "-1" : this.filters.keyword,
-            Type: this.filters.Type,
-            StTime: this.filters.StTime,
-            EndTime: this.filters.EndTime == "" ? "-1" : this.filters.EndTime
-          })
-          .then(
-            function (response) {
-              var status = response.data.Status;
-              if (status === 1) {
-                this.orderList = response.data.Result.List;
-                this.pageCount = response.data.Result.Page;
-              } else if (status === 40001) {
-                this.$message({
-                  showClose: true,
-                  type: "warning",
-                  message: response.data.Result
-                });
-                setTimeout(() => {
-                  tt.$router.push({
-                    path: "/login"
-                  });
-                }, 1500);
-              }
-            }.bind(this)
-          )
-          // 请求error
-          .catch(
-            function (error) {
-              this.$notify.error({
-                title: "错误",
-                message: "错误：请检查网络"
-              });
-            }.bind(this)
-          );
-      },
-      //关键字搜索
-      getUsers() {
-        this.getInfo();
-        // console.log(this.filters)
-      },
-      // 分页
-      handleCurrentChange(val) {
-        this.pageIndex = val;
-        this.getInfo();
+    cardClick(index, row) {
+      var obj = Object.assign({}, row);
+      this.cardList = obj.Card;
+      console.log(obj);
+    },
+    couponClick(index, row) {
+      var obj = Object.assign({}, row);
+      this.CouponList = obj.Coupon;
+    },
+    Sex(row, status) {
+      var sex = row[status.property];
+      switch (sex) {
+        case 0:
+          return (sex = "男");
+          break;
+        default:
+          return (sex = "女");
+          break;
       }
     },
-    mounted() {
-      // this.getInfo();
+    /*
+          1、获取列表 渲染列表
+          2、搜索关键字
+          3、分页
+      */
+    getInfo() {
+      this.$http
+        .get("/sps/api/BackUser/BackUserList", {
+          params: {
+            Token: getCookie("token"),
+            pageIndex: this.pageIndex,
+            pageSize: this.pageSize,
+            Keyword: this.filters.keyword == "" ? "-1" : this.filters.keyword
+          }
+        })
+        .then(
+          function(response) {
+            var status = response.data.Status;
+            if (status === 1) {
+              this.orderList = response.data.Result.list;
+              this.pageCount = response.data.Result.page;
+            } else if (status === 40001) {
+              this.$message({
+                showClose: true,
+                type: "warning",
+                message: response.data.Result
+              });
+              setTimeout(() => {
+                tt.$router.push({
+                  path: "/login"
+                });
+              }, 1500);
+            }
+          }.bind(this)
+        )
+        // 请求error
+        .catch(
+          function(error) {
+            this.$notify.error({
+              title: "错误",
+              message: "错误：请检查网络"
+            });
+          }.bind(this)
+        );
+    },
+    //关键字搜索
+    getUsers() {
+      this.getInfo();
+    },
+    // 分页
+    handleCurrentChange(val) {
+      this.pageIndex = val;
+      this.getInfo();
+    },
+    handleEdit(index, row) {
+      //
+      if (row.IsLock) {
+        this.$confirm("确认解除锁定吗？", "提示", {}).then(() => {
+          this.$http
+            .get("/sps/api/BackUser/Islock", {
+              params: {
+                token: getCookie("token"),
+                id: row.ID
+              }
+            })
+            .then(
+              function(response) {
+                this.editLoading = false;
+                var status = response.data.Status;
+                if (status === 1) {
+                  this.getInfo();
+                } else if (status === 40001) {
+                  this.$message({
+                    showClose: true,
+                    type: "warning",
+                    message: response.data.Result
+                  });
+                  setTimeout(() => {
+                    tt.$router.push({
+                      path: "/login"
+                    });
+                  }, 1500);
+                } else {
+                  this.$message({
+                    showClose: true,
+                    type: "warning",
+                    message: response.data.Result
+                  });
+                }
+              }.bind(this)
+            )
+            // 请求error
+            .catch(
+              function(error) {
+                this.$notify.error({
+                  title: "错误",
+                  message: "错误：请检查网络"
+                });
+              }.bind(this)
+            );
+        });
+      } else {
+        this.$confirm("确认锁定该用户吗？", "提示", {}).then(() => {
+          this.$http
+            .get("/sps/api/BackUser/Islock", {
+              params: {
+                token: getCookie("token"),
+                id: row.ID
+              }
+            })
+            .then(
+              function(response) {
+                this.editLoading = false;
+                var status = response.data.Status;
+                if (status === 1) {
+                  this.getInfo();
+                } else if (status === 40001) {
+                  this.$message({
+                    showClose: true,
+                    type: "warning",
+                    message: response.data.Result
+                  });
+                  setTimeout(() => {
+                    tt.$router.push({
+                      path: "/login"
+                    });
+                  }, 1500);
+                } else {
+                  this.$message({
+                    showClose: true,
+                    type: "warning",
+                    message: response.data.Result
+                  });
+                }
+              }.bind(this)
+            )
+            // 请求error
+            .catch(
+              function(error) {
+                this.$notify.error({
+                  title: "错误",
+                  message: "错误：请检查网络"
+                });
+              }.bind(this)
+            );
+        });
+      }
     }
-  };
-
+  },
+  mounted() {
+    this.mainurl = mainurl;
+    this.getInfo();
+  }
+};
 </script>
 <style scoped>
-  /* 面包屑 */
+/* 面包屑 */
 
-  .crumb {
-    height: 36px;
-    line-height: 36px;
-  }
+.crumb {
+  height: 36px;
+  line-height: 36px;
+}
 
-  .block {
-    text-align: center;
-    padding: 20px 0;
-  }
+.block {
+  text-align: center;
+  padding: 20px 0;
+}
 
-  /* 弹出框 */
+/* 弹出框 */
 
-  .el-dialog__body .el-form-item {
-    width: 60%;
-    margin-left: calc(50% - 30%);
-  }
-
+.el-dialog__body .el-form-item {
+  width: 60%;
+  margin-left: calc(50% - 30%);
+}
 </style>
--- static/urlf.js
+++ static/urlf.js
@@ -1,4 +1,4 @@
-var mainurl = "http://hxm.nbxuanma.com"
+var mainurl = "http://192.168.1.125/Sps/"
 
 function setCookie(name, value) {
 	var Days = 30;
